cd5a28d1d6c4acea734e421093c9065248de3532
Jijidici <jdefaye.jijidici@gmail.com>
2014-01-28 14:42:00 +0100

    Add INKFluidParticleSystem attributes

--- InkParty/includes/physics/INKFluidParticleSystem.h
+++ InkParty/includes/physics/INKFluidParticleSystem.h
@@ -12,7 +12,16 @@ public:
 	INKFluidParticleSystem();
 	~INKFluidParticleSystem();
 
+	void	setSpringRigidity(float fRigidity) { _fSpringRigidity = fRigidity; }
+	void	setSpringLength(float fLength) { _fSpringLength = fLength; }
+	void	setBrakeCoef(float fCoef) { _fBrakeCoef = fCoef; }
+	void	setInfluenceDelta(float fDelta) { _fInfluenceDelta = fDelta; }
 
+protected:
+	float	_fSpringRigidity;
+	float	_fSpringLength;
+	float	_fBrakeCoef;
+	float	_fInfluenceDelta;
 };
 
 #endif //__INK_FLUID_PARTICLE_SYSTEM_H__
