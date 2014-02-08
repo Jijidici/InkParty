1bc5755da17e09f4c35870cf94fbbc61a3126706
Jijidici <jdefaye.jijidici@gmail.com>
2014-02-08 21:55:33 +0100

    Set square frame

--- InkParty/src/system/INKApp.cpp
+++ InkParty/src/system/INKApp.cpp
@@ -43,7 +43,7 @@ void INKApp::onEvent(INKEvent* pE) {
 
 void INKApp::init() {
 	//frame
-	_pMainFrame = new INKFrame("INK PARTY !", 400, 600, 32);
+	_pMainFrame = new INKFrame("INK PARTY !", 600, 600, 32);
 	_pMainFrame->init(60);
 
 	//events
@@ -54,62 +54,13 @@ void INKApp::init() {
 	_pCamera = new INKCamera(_pMainFrame->getRatio());
 	INKRenderer::getInstance()->setCurrentCamera(_pCamera);
 
-	std::vector<glm::vec3> quad1;
-	quad1.push_back(glm::vec3(-10.f,	-12.f, 0.f));
-	quad1.push_back(glm::vec3( 10.f,	-12.f, 0.f));
-	quad1.push_back(glm::vec3( 10.f,	-13.f, 0.f));
-	quad1.push_back(glm::vec3(-10.f,	-13.f, 0.f));
-
-	std::vector<glm::vec3> quad2;
-	quad2.push_back(glm::vec3(-10.f, -4.f, 0.f));
-	quad2.push_back(glm::vec3(-2.f, -14.f, 0.f));
-	quad2.push_back(glm::vec3(-3.5f, -14.f, 0.f));
-	quad2.push_back(glm::vec3(-10.f, -5.5f, 0.f));
-
-	std::vector<glm::vec3> quad3;
-	quad3.push_back(glm::vec3(2.f, -14.f, 0.f));
-	quad3.push_back(glm::vec3(3.5f, -4.f, 0.f));
-	quad3.push_back(glm::vec3(4.5f, -4.f, 0.f));
-	quad3.push_back(glm::vec3(3.f, -14.f, 0.f));
-
-	std::vector<glm::vec3> quad4;
-	quad4.push_back(glm::vec3(-10.f,	5.f, 0.f));
-	quad4.push_back(glm::vec3( 1.f,		3.f, 0.f));
-	quad4.push_back(glm::vec3( 1.f,		2.f, 0.f));
-	quad4.push_back(glm::vec3(-10.f,	4.f, 0.f));
-
-	std::vector<glm::vec3> quad5;
-	quad5.push_back(glm::vec3( -1.f,	-3.f, 0.f));
-	quad5.push_back(glm::vec3( 10.f,	2.f, 0.f));
-	quad5.push_back(glm::vec3( 10.f,	1.f, 0.f));
-	quad5.push_back(glm::vec3( -1.f,	-4.f, 0.f));
-
-	std::vector<glm::vec3> quad6;
-	quad6.push_back(glm::vec3(-10.f,	12.f, 0.f));
-	quad6.push_back(glm::vec3( -9.f,	12.f, 0.f));
-	quad6.push_back(glm::vec3( -9.f,	3.f, 0.f));
-	quad6.push_back(glm::vec3( -10.f,	3.f, 0.f));
-
-	std::vector<glm::vec3> quad7;
-	quad7.push_back(glm::vec3(  9.f,	12.f, 0.f));
-	quad7.push_back(glm::vec3( 10.f,	12.f, 0.f));
-	quad7.push_back(glm::vec3(  10.f,	1.f, 0.f));
-	quad7.push_back(glm::vec3(  9.f,	1.f, 0.f));
-
 	_pPartSystem = new INKFluidParticleSystem(20, static_cast<int>(20.f*_pMainFrame->getRatio()), 0.5f);
 	_pPartSystem->setSpringRigidity(5.f, 2.f);
 	_pPartSystem->setSpringLengths(0.6f, 0.3f);
 	_pPartSystem->setBrakeCoef(0.000001f);
 	_pPartSystem->setInfluenceDelta(0.2f);
-	_pPartSystem->addParticles(512, 0.3f);
+	_pPartSystem->addParticles(256, 0.3f);
 	_pPartSystem->addForce(new INKConstantForce(glm::vec3(0.f, -3.f, 0.f)));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad1, 1.f));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad2, 1.f));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad3, 1.f));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad4, 1.f));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad5, 1.f));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad6, 1.f));
-	_pPartSystem->addSolid(new INKPhysicSolid(quad7, 1.f));
 	INKRenderer::getInstance()->add(_pPartSystem);
 
 	//test zone
